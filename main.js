e5698669ff7fb3c9585df8602728d5f25fd79f11
happylittleflowers <145370707+happylittleflowers@users.noreply.github.com>
2023-11-20 10:04:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// EVENT LISTENERS
+
+// Event listeners
+document.getElementById("btn1").addEventListener("click", function1);
+document.getElementById("btn2").addEventListener("click", function2);
+document.getElementById("btn3").addEventListener("click", function3);
+document.getElementById("btn4").addEventListener("click", function4);
+document.getElementById("btn5").addEventListener("click", function5);
+
+// Event functions
+function function1() {
+  alert("Leg!");
+}
+
+function function2() {
+  alert("Disguise!");
+}
+
+function function3() {
+  alert("There!");
+}
+
+function function4() {
+  alert("No gain!");
+}
+
+function function5() {
+  alert("both worlds!");
+}
